b003004a7843a7ccb1f43314e3fbefb46cff4a3a
BobJiang <374123140@qq.com>
2021-02-22 18:43:59 +0800

    访问主页

---
+++ crowdfunding07-member-parent/atcrowdfunding12-member-authentication-consumer/src/main/java/com/atguigu/crowd/handler/PortalHandler.java
@@ -0,0 +1,26 @@
+package com.atguigu.crowd.handler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author BobJiang
+ * @version 1.0
+ * @date 2021-02-22 17:41
+ */
+@Controller
+public class PortalHandler {
+
+    // 首页
+    @RequestMapping("/")
+    public String showPortalPage(){
+//        // 调用MySQLRemoteService提供的方法查询首页要显示的数据
+//        ResultEntity<List<PortalTypeVO>> resultEntity = mySQLRemoteService.getPortalTypeProjectDataRemote();
+//        // 如果操作成功，将得到的list加入请求域
+//        if (ResultEntity.SUCCESS.equals(resultEntity.getResult())){
+//            List<PortalTypeVO> portalTypeVOList = resultEntity.getData();
+//            modelMap.addAttribute(CrowdConstant.ATTR_NAME_PORTAL_TYPE_LIST,portalTypeVOList);
+//        }
+        return "portal";
+    }
+}
